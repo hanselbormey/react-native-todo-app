d8ca196580d79882dc531b91c91c62b29f8c9e13
Hansel Bormey <hanselbormey@gmail.com>
2023-08-24 22:01:12 -0400

    Update TodoList

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import { FlatList, Text, View } from 'react-native';
-import { TODOS } from '../utils/mockData';
 import TodoItem from './TodoItem';
 
-export default function TodoList() {
-  const [data, setData] = React.useState(TODOS);
-
-  const handleCheck = (id, event) => {
-    const todos = data.map((item) => {
-      if (item.id === id) {
-        item.isCompleted = event;
-        return item;
-      }
-      return item;
-    });
-
-    setData(todos);
-  };
-
+export default function TodoList({ data, onCheck }) {
   return (
     <FlatList
       data={data}
       keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => <TodoItem {...item} onCheck={handleCheck} />}
+      renderItem={({ item }) => <TodoItem {...item} onCheck={onCheck} />}
     />
   );
 }
