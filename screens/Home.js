f16d4cd5093708c293a71e637125dc0e4d098d74
Hansel Bormey <hanselbormey@gmail.com>
2023-08-24 22:02:30 -0400

    Update  home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,59 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import TodoList from '../components/TodoList';
 import theme from '../theme';
+import { TODOS } from '../utils/mockData';
+import { isToday } from '../utils/helpers';
 
 export default function HomeScreen() {
+  const [data, setData] = React.useState([]);
+  const [isHidden, setIsHidden] = React.useState(false);
+
+  const [todayTodos, setTodayTodos] = React.useState();
+  const [upcomingTodos, setUpcomingTodos] = React.useState();
+
+  React.useEffect(() => {
+    setData(TODOS.sort((a, b) => a.isCompleted - b.isCompleted));
+  }, []);
+
+  React.useEffect(() => {
+    setTodayTodos(data?.filter((item) => isToday(new Date(item.date))));
+    setUpcomingTodos(data?.filter((item) => !isToday(new Date(item.date))));
+  }, [data]);
+
+  const handleCheck = (id, event) => {
+    const todos = data.map((item) => {
+      if (item.id === id) {
+        item.isCompleted = event;
+        return item;
+      }
+      return item;
+    });
+
+    setData(todos);
+  };
+
+  const handleShow = () => {
+    isHidden
+      ? setData(TODOS)
+      : setData(TODOS.filter((item) => !item.isCompleted));
+    setIsHidden(!isHidden);
+  };
+
   return (
     <View style={styles.container}>
-      <Image
-        source={require('../assets/man_avatar.jpg')}
-        style={styles.avatar}
-      />
-      <Text style={styles.title}>Today</Text>
+      <Image source={require('../assets/avatar.jpg')} style={styles.avatar} />
+      <View style={styles.hiddenContainer}>
+        <Text style={styles.title}>Today</Text>
+        <TouchableOpacity onPress={handleShow}>
+          <Text style={styles.hiddeBtn}>
+            {!isHidden ? 'Hide completed' : 'Show all'}
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <TodoList data={todayTodos} onCheck={handleCheck} />
       <Text style={styles.title}>Upcoming</Text>
-      <TodoList />
+      <TodoList data={upcomingTodos} onCheck={handleCheck} />
     </View>
   );
 }
@@ -26,9 +68,20 @@ const styles = StyleSheet.create({
   },
   container: {
     padding: 10,
+    flex: 1,
+  },
+  hiddenContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  hiddeBtn: {
+    color: theme.colors.primary,
   },
   title: {
     fontSize: theme.fontSizes.heading,
     fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 24,
   },
 });
