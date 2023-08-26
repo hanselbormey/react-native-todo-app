f85404b15db51890c0a47e29bf72ffcd6ba001b5
Hansel Bormey <hanselbormey@gmail.com>
2023-08-25 22:45:15 -0400

    Add navigation stuff

--- App.js
+++ App.js
@@ -1,32 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import HomeScreen from './screens/Home';
+import AddTodo from './screens/AddTodo';
 
 function SafeArea({ children }) {
   const insets = useSafeAreaInsets();
   return <View style={{ flex: 1, paddingTop: insets.top }}>{children}</View>;
 }
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeArea>
-        <HomeScreen />
-        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                headerTitleAlign: 'center',
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Add"
+              component={AddTodo}
+              options={{
+                headerTitleAlign: 'center',
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeArea>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 8,
-  },
-  safeArea: {
-    flex: 1,
-  },
-});
