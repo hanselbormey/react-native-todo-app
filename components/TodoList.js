caff54ae7438ffc45937ce24675623ff463181e5
Hansel Bormey <hanselbormey@gmail.com>
2023-08-30 21:18:00 -0400

    Remove unused imports

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, Text, View } from 'react-native';
+import { FlatList } from 'react-native';
 import TodoItem from './TodoItem';
 
 export default function TodoList({ data, onCheck }) {
