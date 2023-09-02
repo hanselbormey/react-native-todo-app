ba506791c37cacd09de1c8aa6e1aa2087f9d8e0e
Hansel Bormey <hanselbormey@gmail.com>
2023-09-01 22:56:41 -0400

    Update getTodos query

--- hooks/useGetTodos.js
+++ hooks/useGetTodos.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 
 import db from '../lib/firebase';
 
 export default function useGetTodos() {
   const [todos, setTodos] = React.useState();
 
-  React.useEffect(() => {
+  /*   React.useEffect(() => {
     async function getData() {
       const querySnapshot = await getDocs(collection(db, 'todo'));
 
@@ -20,6 +20,24 @@ export default function useGetTodos() {
       setTodos(arr);
     }
     getData();
+  }, []); */
+
+  React.useEffect(() => {
+    const collectionRef = collection(db, 'todo');
+    const q = query(collectionRef, orderBy('created_at', 'desc'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      // onSnapshot is a listener that listens to changes in the database in realtime
+      setTodos(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          text: doc.data().text,
+          isCompleted: doc.data().is_completed,
+          date: doc.data().date.toDate(),
+        }))
+      );
+    });
+    return unsubscribe; // unsubscribe from the listener when the component is unmounting
+    // because it avoids memory leaks
   }, []);
 
   return { todos };
