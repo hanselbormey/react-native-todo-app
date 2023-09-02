e98f3d6aa93730f750730fd2f24ca8643f9e11df
Hansel Bormey <hanselbormey@gmail.com>
2023-09-01 23:45:13 -0400

    Add addTodo functionality

--- screens/AddTodo.js
+++ screens/AddTodo.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { addDoc, collection } from 'firebase/firestore';
+
+import { useNavigation } from '@react-navigation/native';
+
+import AddTodoForm from '../components/AddTodoForm';
+import db from '../lib/firebase';
 
 export default function AddTodo() {
+  const navigation = useNavigation();
+
+  const handleAdd = async (todo) => {
+    const docRef = await addDoc(collection(db, 'todo'), todo);
+    navigation.goBack();
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Add Todo</Text>
+    <View style={{ flex: 1, alignItems: 'center', marginTop: 40 }}>
+      <AddTodoForm onSubmit={handleAdd} />
     </View>
   );
 }
