5863be4a6181a5f58ab7039a21baf8636efc3fa6
Hansel Bormey <hanselbormey@gmail.com>
2023-09-02 22:08:01 -0400

    Introduce datePicker component

--- components/AddTodoForm.js
+++ components/AddTodoForm.js
@@ -1,15 +1,45 @@
 import React from 'react';
-import { Text, TextInput, View, StyleSheet } from 'react-native';
+import {
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Pressable,
+} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 import theme from '../theme';
-import { TouchableOpacity } from 'react-native';
 
 export default function AddTodoForm({ onSubmit }) {
   const [text, setText] = React.useState();
+  const [date, setDate] = React.useState(new Date());
+  const [mode, setMode] = React.useState('date');
+  const [show, setShow] = React.useState(false);
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
 
   const handleSubmit = () => {
     onSubmit({
       text,
-      date: new Date(),
+      date,
       created_at: new Date(),
       is_completed: false,
     });
@@ -24,6 +54,33 @@ export default function AddTodoForm({ onSubmit }) {
           onChangeText={(newText) => setText(newText)}
           defaultValue={text}
         />
+      </View>
+
+      <View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={styles.label}>selected: </Text>
+            <Text>{date.toLocaleString()}</Text>
+          </View>
+
+          <View>
+            <Pressable onPress={showDatepicker} style={styles.btnPicker}>
+              <Text>Set Date</Text>
+            </Pressable>
+            <Pressable onPress={showTimepicker} style={styles.btnPicker}>
+              <Text>Set Time</Text>
+            </Pressable>
+          </View>
+        </View>
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={mode}
+            is24Hour={false}
+            onChange={onChange}
+          />
+        )}
       </View>
 
       <TouchableOpacity onPress={handleSubmit} style={styles.btn}>
@@ -59,5 +116,11 @@ const styles = StyleSheet.create({
   },
   btnText: {
     color: theme.colors.palette.light,
+  },
+  btnPicker: {
+    marginBottom: 8,
+    backgroundColor: theme.colors.palette.light,
+    padding: 10,
+    borderRadius: 8,
   },
 });
